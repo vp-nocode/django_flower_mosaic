970790cf1f8c5bdc6847a5c448eeb1db66535a6c
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-12-06 13:12:23 +0500

    fixed tg_bot

--- tg_bot.py
+++ tg_bot.py
@@ -3,65 +3,83 @@ import requests
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 TOKEN = os.getenv('TGB_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-# URL API вашего Django веб-приложения
-DJANGO_API_URL = 'http://your_django_server/api/order_status/'
+DJANGO_API_URL = 'http://digidobu.beget.tech/api/order_status/'
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
+def create_main_menu():
     markup = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
     btn1 = telebot.types.KeyboardButton('Как заказать букет')
     btn2 = telebot.types.KeyboardButton('Проверить статус заказа')
     markup.add(btn1, btn2)
+    return markup
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = create_main_menu()
     bot.send_message(message.chat.id, "Добро пожаловать в нашу цветочную студию!", reply_markup=markup)
 
-
-# Обработчик команды /help
 @bot.message_handler(commands=['help'])
 def send_help(message):
     help_text = (
         "Вот список доступных команд:\n"
         "/start - Начать взаимодействие с ботом\n"
         "/help - Получить помощь и список команд\n"
+        "/info - Как заказать букет\n"
+        "/status <номер заказа> - Проверить статус заказа\n"
         "Вы также можете использовать кнопки для навигации:\n"
         "- Как заказать букет\n"
         "- Проверить статус заказа\n"
     )
     bot.send_message(message.chat.id, help_text)
 
+@bot.message_handler(commands=['status'])
+def handle_status_command(message):
+    command_parts = message.text.split()
+    if len(command_parts) > 1:
+        order_id = command_parts[1]
+        check_order_status_by_id(message, order_id)
+    else:
+        bot.send_message(message.chat.id, "Пожалуйста, введите номер вашего заказа:")
+        bot.register_next_step_handler(message, check_order_status)
 
-# Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    if message.text == 'Как заказать букет':
-        # bot.send_message(message.chat.id, "Для заказа букетов перейдите на наш сайт: https://flower-mosaic.com")
-        bot.send_message(message.chat.id, "Для заказа букетов перейдите на наш сайт: http://127.0.0.1:8000/")
+    if message.text == 'Как заказать букет' or message.text == '/info':
+        bot.send_message(message.chat.id, "Для заказа букетов перейдите на наш сайт: http://digidobu.beget.tech/")
     elif message.text == 'Проверить статус заказа':
         bot.send_message(message.chat.id, "Пожалуйста, введите номер вашего заказа:")
         bot.register_next_step_handler(message, check_order_status)
     else:
         bot.send_message(message.chat.id,
-        "Пожалуйста, выберите одну из предложенных опций или используйте /help для получения списка команд.")
+                         "Пожалуйста, выберите одну из предложенных опций или используйте /help для получения списка команд.")
 
-# Функция для проверки статуса заказа
 def check_order_status(message):
     order_id = message.text
-    response = requests.get(f"{DJANGO_API_URL}{order_id}/")
+    check_order_status_by_id(message, order_id)
 
+def check_order_status_by_id(message, order_id):
+    response = requests.get(f"{DJANGO_API_URL}{order_id}/")
     if response.status_code == 200:
         order_info = response.json()
-        items_info = "\n".join([
-            f"{item['product_name']}: {item['quantity']} шт. по {item['price']} руб."
-            for item in order_info['items']
-        ])
-        bot.send_message(message.chat.id, f"Заказ №{order_info['order_id']}:\nСтатус: {order_info['status']}\n{items_info}")
+        bot.send_message(message.chat.id,
+                         f"Заказ №{order_info['id']}:\nСтатус: {order_info['status']}\nАдрес доставки: {order_info['delivery_address']}")
+
+        for item in order_info['items']:
+            image_url = f"http://digidobu.beget.tech/flower_mosaic/media/{item['image']}"  # Предполагается, что изображения хранятся в этой директории
+            try:
+                bot.send_photo(message.chat.id, image_url,
+                               caption=f"{item['product_name']}: {item['quantity']} шт. по {item['price']} руб.")
+            except Exception as e:
+                bot.send_message(message.chat.id,
+                                 f"{item['product_name']}: {item['quantity']} шт. по {item['price']} руб.\n(Изображение недоступно)")
     else:
         bot.send_message(message.chat.id, "Заказ с таким номером не найден. Пожалуйста, попробуйте снова.")
 
-# Запуск бота
+    # Повторное отображение основного меню
+    markup = create_main_menu()
+    bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
+
 bot.polling()
